26d4a2310b759bb1b0e9f70b1a2868ee717afc02
Jennifer Davila <pazjenni1331@gmail.com>
2022-01-26 23:59:14 -0600

    fixed overflow in div.

--- src/components/Driver/driver-info.js
+++ src/components/Driver/driver-info.js
@@ -5,9 +5,7 @@ import data from "../../assets/data/mission.json";
 const driverData = data.map((data) => {
   return (
     <div key={data.driver} className="container driver-info">
-      <h2 className="driver-name" style={{ marginBottom: "40px" }}>
-        {data.driver.name}
-      </h2>
+      <h2 className="driver-name">{data.driver.name}</h2>
       <hr className="w-25 top-line"></hr>
       <p className="driver-bio">{data.driver.bio}</p>
     </div>
